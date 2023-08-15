44ee9158a651411c2e7ec6dd45ff62fb01a770c3
AlhamAhmad12 <141309691+AlhamAhmad12@users.noreply.github.com>
2023-08-15 00:55:29 -0700

    modify exe.ts

--- exe4.ts
+++ exe4.ts
@@ -1,3 +1,7 @@
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+
+
 let Famous_person = "Quaid e azam"
 let message = ( Famous_person +` once said, "Think 100 times before you take a decision, But once that decision is taken, stand by it as one man."`) 
 console.log(message)   
--- exe6.ts
+++ exe6.ts
@@ -1,3 +1,4 @@
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
 
 //Addition
 const addition = 4+4;
