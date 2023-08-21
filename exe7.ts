23dfa7ccb7c9d99846af80c2946af5e1f63a6aab
AlhamAhmad12 <141309691+AlhamAhmad12@users.noreply.github.com>
2023-08-21 13:27:08 +0500

    modify.exe7

--- exe7.ts
+++ exe7.ts
@@ -1,12 +1,22 @@
 
 
 
-console.log("_".repeat(80));
-console.log(5 + 3)
-console.log("_".repeat(80));
-console.log(16 - 8)
-console.log("_".repeat(80));
-console.log(4 * 4)
-console.log("_".repeat(80));
-console.log(32 / 4)
-console.log("_".repeat(80));
+// console.log("_".repeat(80));
+// console.log(5 + 3)
+// console.log("_".repeat(80));
+// console.log(16 - 8)
+// console.log("_".repeat(80));
+// console.log(4 * 4)
+// console.log("_".repeat(80));
+// console.log(32 / 4)
+// console.log("_".repeat(80));
+
+console.log("-------------------------------------");
+console.log(5 + 3);
+console.log("-------------------------------------");
+console.log(16 - 8);
+console.log("-------------------------------------");
+console.log(4 * 2);
+console.log("-------------------------------------");
+console.log(64 / 8);
+console.log("-------------------------------------");
