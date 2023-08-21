aca98a5cc793c3a85b6b466eff4356cf341d72e3
AlhamAhmad12 <141309691+AlhamAhmad12@users.noreply.github.com>
2023-08-21 14:14:22 +0500

    modify exe8

--- exe8.ts
+++ exe8.ts
@@ -1,5 +1,5 @@
 // Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
 
 let number = 1;
-let message1 = `my favorite number is ${message1}` ;
+let message1 = `my favorite number is: ${number}` ;
 console.log(message1);
