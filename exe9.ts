8a688d4d27a9427f2f91bc879703e323a9c6aebd
AlhamAhmad12 <141309691+AlhamAhmad12@users.noreply.github.com>
2023-08-21 14:13:51 +0500

    exe8 and exe9

--- exe9.ts
+++ exe9.ts
@@ -1,5 +1,9 @@
-// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
 
-const favoriteNumber = 1;
-const message2 = `my favorite number: ${favoriteNumber}`;
-console.log(message2);
+//Alham Ahmad;
+//this program will be returned my name and age;
+let Myname = (Name: string, num : number) => {
+
+    return `my name is ${Name} and my age is ${num}`
+}
+console.log(Myname("Alham Ahmad",20))
