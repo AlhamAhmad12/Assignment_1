3268720802e627cec047aa6a865b96b1d1a5bfc8
AlhamAhmad12 <141309691+AlhamAhmad12@users.noreply.github.com>
2023-08-21 13:42:16 +0500

    modify

--- exe9.js
+++ exe9.js
@@ -1,1 +1,4 @@
-"use strict";
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+var favoriteNumber = 1;
+var message2 = "my favorite number: ".concat(favoriteNumber);
+console.log(message2);
