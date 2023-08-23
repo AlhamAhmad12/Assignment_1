a88c06881af735f5727201305f16c056b8e38d2a
AlhamAhmad12 <141309691+AlhamAhmad12@users.noreply.github.com>
2023-08-24 01:05:39 +0500

    a

---
+++ exe21.ts
@@ -0,0 +1,29 @@
+   
+//They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+
+let cars = [ {
+    name: "honda",
+    color: "white",
+    modle: 2023
+  },
+  {
+    name: "supra",
+    color: "yellow",
+    modle: 2020
+  },
+  {
+    name: "BMW",
+    color: "black",
+    modle: 2022
+  },
+  {
+    name: "ferrari",
+    color: "green",
+    modle: 2018
+  }
+  ];
+  console.log("car names modle and color:")
+  for(car of cars){
+    console.log(`car ${car.name} modle ${car.modle} and color ${car.color}`)
+  }
