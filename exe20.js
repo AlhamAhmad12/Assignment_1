c7a7979d41ff11b0502adc9acc6a28f1a4459848
AlhamAhmad12 <141309691+AlhamAhmad12@users.noreply.github.com>
2023-08-24 01:01:14 +0500

    a

--- exe20.js
+++ exe20.js
@@ -1,6 +1,6 @@
 //Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
 // Create an array of programming languages
-var mountainsList = ["Annapurna II", "Manaslu", "Cho Oyu", "	Makalu", "Lhotse", "K2", "Mount Everest"];
+var mountainsList = ["Annapurna II", "Manaslu", "Cho Oyu", "Makalu", "Lhotse", "K2", "Mount Everest"];
 // Print the list of programming mountains
 console.log("List of mountains:");
 for (var _i = 0, mountainsList_1 = mountainsList; _i < mountainsList_1.length; _i++) {
