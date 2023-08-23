40f3ae58f9eb7644aaf71d55d59d30af37d98212
AlhamAhmad12 <141309691+AlhamAhmad12@users.noreply.github.com>
2023-08-24 00:47:27 +0500

    all

---
+++ exe20.ts
@@ -0,0 +1,10 @@
+//Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+// Create an array of programming languages
+const mountains: string[] = ["Annapurna II", "Manaslu", "Cho Oyu", "	Makalu", "Lhotse", "K2", "Mount Everest"];
+  
+  // Print the list of programming mountains
+  console.log("List of mountains:");
+  for (const mountain of mountains) {
+    console.log(mountains);
+  }
