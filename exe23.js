c646c9eb2cacc45c6209179214f7c0f560b25898
AlhamAhmad12 <141309691+AlhamAhmad12@users.noreply.github.com>
2023-08-24 13:23:43 +0500

    a

--- exe23.js
+++ exe23.js
@@ -1,7 +1,16 @@
-var number1 = 42;
-var string = "hello";
-var boolTrue = true;
-var name12 = "alham";
+"use strict";
+/*
+Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.")
+console.log(car == 'subaru')
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+*/
+let number1 = 42;
+let string = "hello";
+let boolTrue = true;
+let name12 = "alham";
 console.log("Is number equal to 42? I predict True.");
 console.log(number1 === 42);
 console.log("Is string not equal to 'hello'? I predict false.");
---
+++ exe24.ts
@@ -0,0 +1,73 @@
+/*
+More Conditional Tests: You don’t have to limit the number of tests you create to 10. If you want to try more comparisons, write more tests. Have at least one True and one False result for each of the following:
+• Tests for equality and inequality with strings
+• Tests using the lower case function
+• Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+• Tests using "and" and "or" operators
+• Test whether an item is in a array
+• Test whether an item is not in a array 
+*/
+
+// Tests for equality and inequality with strings
+let country = "Pakistan"; 
+console.log("is country is equal to 'Pakistan'? I predict True.")
+console.log(country === "Pakistan")
+
+console.log("is country is not equal to 'Pakistan'? I predict False.")
+console.log(country !== "Pakistan")
+
+// Tests using the lower case function
+let name11 = "alham";
+
+console.log("Is name11 in lowercase equal to 'alham'? I predict True.")
+console.log(name11.toLowerCase() === "alham")
+
+console.log("Is name11 in lowercase equal to 'Alham'? I predict false.")
+console.log(name11.toLowerCase() === "Aham")
+
+// Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+
+let number22 = 100;
+
+console.log("is number22 is equal to 100? I predict true")
+console.log(number22 == 100)
+
+console.log("is number22 is not equal to 100? I predict false")
+console.log(number22 !== 100);
+
+//grater than and Less than 
+console.log("is number22 is grater than 60? I predict true" )
+console.log(number22 > 60)
+
+console.log("is number22 is Less than 88? I predict false")
+console.log(number22 < 88)
+
+//Grater than and equal to
+console.log("is number22 is grater than and equal to 99 ? I predict false")
+console.log(number22 >= 99)
+ 
+//Less than and equal to
+console.log("is number22 is Less than and equal to 30? I predict false")
+console.log(number22 <= 30)
+
+//Tests using "and" and "or" operators
+let laptop = true;
+let computer = false;
+
+console.log("Do they have a laptop and a computer? I predict False.");
+console.log(laptop && computer);
+
+console.log("Do they have a laptop or a computer? I predict True.");
+console.log(laptop || computer);
+
+// Test whether an item is in a array
+
+let array = ["BMW", "Audi", "Ferrari", "Honda",];
+
+console.log("is BMW is included in the array? I predict True.");
+console.log(array.includes("BMW"));
+
+// Test whether an item is not in a array
+
+console.log("is Audi is not included in the array ? I predict False");
+console.log(!array.includes("Audi"));
