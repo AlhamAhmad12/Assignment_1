602b5e55f24579507a827ec4e1453a52a9e97f5d
AlhamAhmad12 <141309691+AlhamAhmad12@users.noreply.github.com>
2023-08-24 11:52:13 +0500

    all

--- exe22.js
+++ exe22.js
@@ -1,14 +1,15 @@
+"use strict";
 //Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
 // Create an array of programming languages
-var progLanguages = ["Python", "Java", "Ruby", "Go", "PHP", "TypeScript", "Rust", "C++"];
+const progLanguages = ["Python", "Java", "Ruby", "Go", "PHP", "TypeScript", "Rust", "C++"];
 // invalid index
-var invalidIndex = 11;
+const invalidIndex = 11;
 console.log(invalidIndex);
 //Correct the error 
 if (invalidIndex < progLanguages.length) {
     console.log("Valid index:", progLanguages[invalidIndex]);
 }
 else {
-    console.log("Invalid index, out of bounds.");
+    console.log("Invalid index.");
 }
 ;
---
+++ exe23.ts
@@ -0,0 +1,34 @@
+let number1 = 42;
+let string = "hello";
+let boolTrue = true;
+let name12 = "alham";
+
+console.log("Is number equal to 42? I predict True.");
+console.log(number1 === 42);
+
+console.log("Is string not equal to 'hello'? I predict false.");
+console.log(string !== "hello");
+
+console.log("Is string not equal to 'world'? I predict True.");
+console.log(string !== "world");
+
+console.log("Is number not equal to 42? I predict False.");
+console.log(number1 !== 42);
+
+console.log("Is boolTrue equal to true? I predict True.");
+console.log(boolTrue === true);
+
+console.log("Is name12 not equal to 'alham'? I predict False.");
+console.log(name12 !== "alham");
+
+console.log("Is number greater than 30? I predict True.");
+console.log(number > 30);
+
+console.log("Is boolTrue not equal to true? I predict false.");
+console.log(boolTrue !== true);
+
+console.log("Is name12 equal to 'alham'? I predict true.");
+console.log(name12 === "alham");
+
+console.log("Is number less than or equal to 20? I predict False.");
+console.log(number1 <= 20);
