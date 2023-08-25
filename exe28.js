54b5be5c0746ec64079e99c351cafa8b3572f304
AlhamAhmad12 <141309691+AlhamAhmad12@users.noreply.github.com>
2023-08-25 12:49:08 +0500

    update

--- exe28.js
+++ exe28.js
@@ -1,4 +1,3 @@
-"use strict";
 /*
 Stages of Life: Write an if-else chain that determines a person’s stage of life. Set a value for the variable age, and then:
 • If the person is less than 2 years old, print a message that the person is a baby.
@@ -8,7 +7,7 @@ Stages of Life: Write an if-else chain that determines a person’s stage of lif
 • If the person is at least 20 years old but less than 65, print a message that the person is an adult.
 • If the person is age 65 or older, print a message that the person is an elder.
 */
-let age = 30;
+var age = 30;
 if (age < 2) {
     console.log("The person's is baby.");
 }
