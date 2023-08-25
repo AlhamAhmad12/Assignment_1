eedd2d994e426e76a714916cae8b7e404e2dae43
AlhamAhmad12 <141309691+AlhamAhmad12@users.noreply.github.com>
2023-08-25 13:30:42 +0500

    update

--- exe28.js
+++ exe28.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
 Stages of Life: Write an if-else chain that determines a person’s stage of life. Set a value for the variable age, and then:
 • If the person is less than 2 years old, print a message that the person is a baby.
@@ -7,7 +8,7 @@ Stages of Life: Write an if-else chain that determines a person’s stage of lif
 • If the person is at least 20 years old but less than 65, print a message that the person is an adult.
 • If the person is age 65 or older, print a message that the person is an elder.
 */
-var age = 30;
+let age = 30;
 if (age < 2) {
     console.log("The person's is baby.");
 }
---
+++ exe29.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+• Make a array of your three favorite fruits and call it favorite_fruits.
+
+• Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement, such as You really like bananas!.
+*/
+let favorite_fruits = ["mango", "banana", "strawberry"];
+if (favorite_fruits.includes("mango")) {
+    console.log("You really like mango!");
+}
+if (favorite_fruits.includes("banana")) {
+    console.log("You really like banana!");
+}
+if (favorite_fruits.includes("strawberry")) {
+    console.log("You really like strawberry!");
+}
+if (favorite_fruits.includes("kawi")) {
+    console.log("You really like kawi!");
+}
+if (favorite_fruits.includes("orang")) {
+    console.log("You really like orang!");
+}
