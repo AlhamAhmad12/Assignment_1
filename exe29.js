8db19e151faf6a81956d286149f76b435a460c50
AlhamAhmad12 <141309691+AlhamAhmad12@users.noreply.github.com>
2023-08-25 13:32:10 +0500

    update

--- exe29.js
+++ exe29.js
@@ -1,10 +1,9 @@
-"use strict";
 /*
 • Make a array of your three favorite fruits and call it favorite_fruits.
 
 • Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement, such as You really like bananas!.
 */
-let favorite_fruits = ["mango", "banana", "strawberry"];
+var favorite_fruits = ["mango", "banana", "strawberry"];
 if (favorite_fruits.includes("mango")) {
     console.log("You really like mango!");
 }
