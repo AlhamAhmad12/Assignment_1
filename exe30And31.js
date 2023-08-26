f8f453c685b7997e1de0cef4253d15cf1710b316
AlhamAhmad12 <141309691+AlhamAhmad12@users.noreply.github.com>
2023-08-26 14:49:47 +0500

    update

--- exe30And31.js
+++ exe30And31.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
 Hello Admin: Make a array of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the array, and print a greeting to each user:
 • If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
@@ -6,15 +7,14 @@ No Users: Add an if test to Exercise 28 to make sure the list of users is not em
 • If the list is empty, print the message We need to find some users!
 • Remove all of the usernames from your array, and make sure the correct message is printed.
   */
-var usernames = ["admin", "alham", "hurira", "wadoon"];
+let usernames = ["admin", "Alham", "Hurira", "Wadoon"];
 if (usernames.length > 0) {
-    for (var _i = 0, usernames_1 = usernames; _i < usernames_1.length; _i++) {
-        var username = usernames_1[_i];
+    for (let username of usernames) {
         if (username === "admin") {
             console.log("Hello admin, would you like tosee a status report?");
         }
         else {
-            console.log("Hello ".concat(username, ", thank you for logging un again."));
+            console.log(`Hello ${username}, thank you for logging un again.`);
         }
     }
 }
@@ -22,4 +22,4 @@ else {
     console.log("We need to find some users!");
 }
 usernames.splice(0, 4);
-console.log("we need to find some users! ".concat(usernames));
+console.log(`we need to find some users! ${usernames}`);
