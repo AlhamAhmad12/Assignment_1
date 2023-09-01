ef8479cd0c35d6788962e9a298bbe7061ea7850e
AlhamAhmad12 <141309691+AlhamAhmad12@users.noreply.github.com>
2023-09-01 18:07:56 +0500

    modify

--- exe38.ts
+++ exe38.ts
@@ -1,3 +1,12 @@
+/*
+37 : Cities: Write a function called describe_city() that accepts the name of a city and its country. The function should print a simple sentence, such as Karachi is in Pakistan. Give the parameter for the country a default value. Call your function for three different cities, at least one of which is not in the default country.
+
+38 : City Names: Write a function called city_country() that takes in the name of a city and its country. The function should return a string formatted like this:
+"Lahore, Pakistan"
+Call your function with at least three city-country pairs, and print the value that’s returned.
+*/
+
+
 // Function to describe a city's location
 function describeCity(city: string, country = 'USA') {
     console.log(`${city} is in ${country}.`);
