e21326aebcd6536cdf75bdf86a94cb8a35b4d460
AlhamAhmad12 <141309691+AlhamAhmad12@users.noreply.github.com>
2023-09-01 18:47:33 +0500

    modify

---
+++ exe41.js
@@ -0,0 +1,9 @@
+"use strict";
+// Exercise 41: Magicians
+function show_magicians(magicians) {
+    magicians.forEach((magician) => {
+        console.log(magician);
+    });
+}
+const magicians = ['Apollo', 'Blaine', 'Devant'];
+show_magicians(magicians);
---
+++ exe41.ts
@@ -0,0 +1,9 @@
+// Exercise 41: Magicians
+function show_magicians(magicians: string[]): void {
+    magicians.forEach((magician) => {
+        console.log(magician);
+    });
+}
+
+const magicians: string[] = ['Apollo', 'Blaine', 'Devant'];
+show_magicians(magicians);
